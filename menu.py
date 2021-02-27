e3b1a377120450d8f2e63fb3d730e2094f31d8ee
unknown <dimon.ka2002@gmail.com>
2021-02-27 12:02:48 +0200

    Version 2.0

--- menu.py
+++ menu.py
@@ -4,11 +4,14 @@ menu.py
 """
 
 import sys
-from notebook import Notebook, Note
+from notebook import Notebook
 
 class Menu:
     '''Display a menu and respond to choices when run.'''
     def __init__(self):
+        '''
+        Docstring
+        '''
         self.notebook = Notebook()
         self.choices = {
             "1": self.show_notes,
@@ -19,6 +22,9 @@ class Menu:
         }
 
     def display_menu(self):
+        '''
+        Docstring
+        '''
         print("""
 Notebook Menu
 
@@ -41,6 +47,9 @@ Notebook Menu
                 print("{0} is not a valid choice".format(choice))
 
     def show_notes(self, notes=None):
+        '''
+        Docstring
+        '''
         if not notes:
             notes = self.notebook.notes
         for note in notes:
@@ -48,16 +57,25 @@ Notebook Menu
             note.id, note.tags, note.memo))
 
     def search_notes(self):
+        '''
+        Docstring
+        '''
         note_filter = input("Search for: ")
         notes = self.notebook.search(note_filter)
         self.show_notes(notes)
 
     def add_note(self):
+        '''
+        Docstring
+        '''
         memo = input("Enter a memo: ")
         self.notebook.new_note(memo)
         print("Your note has been added.")
 
     def modify_note(self):
+        '''
+        Docstring
+        '''
         note_id = input("Enter a note id: ")
         memo = input("Enter a memo: ")
         tags = input("Enter tags: ")
@@ -69,6 +87,9 @@ Notebook Menu
                 print('Error')
 
     def quit(self):
+        '''
+        Docstring
+        '''
         print("Thank you for using your notebook today.")
         sys.exit(0)
 
